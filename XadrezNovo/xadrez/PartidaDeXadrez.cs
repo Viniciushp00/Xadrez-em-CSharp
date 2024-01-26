7f9e8de56e84314c7d9fe57ba70c2d6a7e1ed4d6
Marcos Vinicius <viniciushp00@gmail.com>
2024-01-26 17:42:01 -0300

    Criação da jogada especial promoção (Dama)

--- XadrezNovo/xadrez/PartidaDeXadrez.cs
+++ XadrezNovo/xadrez/PartidaDeXadrez.cs
@@ -139,6 +139,23 @@ namespace xadrez
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
+
+            Peca p = tab.peca(destino);
+
+            //Jogada Especial Promoção
+            if(p is Peao)
+            {
+                if ((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarpeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -155,7 +172,7 @@ namespace xadrez
                 mudaJogador();
             }
 
-            Peca p = tab.peca(destino);
+            
 
             //Jogada especial En Passant
             if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
